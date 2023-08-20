cf9df813b012e0f0be1f953b5b2e3376e090e07a
NiMs <abdallhmoustafa295@gmail.com>
2023-08-20 16:35:41 +0300

    make search

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:wallpaper_anime/routes/routes.dart';
 
 void main() {
   runApp(MyApp());
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
 }
 
 class MyApp extends StatelessWidget {
